89d3887ff103ce4359e5201654c602d383d71e9a
sommiecodes <chisom.miracle001@gmail.com>
2022-08-26 16:57:13 +0100

    Input JavaScript

---
+++ main.js
@@ -0,0 +1,12 @@
+const integer = 4;
+console.log(integer);
+
+const today = Date();
+console.log(today);
+
+const age = 15;
+let text;
+if (age > 18) text = "Too young to buy alcohol";
+else text = "Buy alcohol"
+console.log(text);
+
